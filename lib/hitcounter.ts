c2d516c4d4142651ec5c8d26134c230f74a7c53a
scosh <scotia.draven@ecoatm.com>
2022-08-08 23:35:32 -0700

    sorted TableViewer by hits

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -9,17 +9,23 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
-
     /** allows accessing the counter function */
     public readonly handler: lambda.Function;
+
+    //** the hit counter table */
+    public readonly table: dynamodb.Table;
 
     constructor(scope: Construct, id: string, props: HitCounterProps) {
         super(scope, id);
 
         const table = new dynamodb.Table(this, 'Hits', {
-            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+            partitionKey: {
+                name: 'path', 
+                type: dynamodb.AttributeType.STRING
+            }
         });
-        
+        this.table = table;
+
         this.handler = new lambda.Function(this, 'HitCounterHandler', {
             runtime: lambda.Runtime.NODEJS_14_X,
             handler: 'hitcounter.handler',
@@ -29,6 +35,7 @@ export class HitCounter extends Construct {
                 HITS_TABLE_NAME: table.tableName
             }
         });
+        
         // grant the lambda role read/write permissions to our table
         table.grantReadWriteData(this.handler);
 
